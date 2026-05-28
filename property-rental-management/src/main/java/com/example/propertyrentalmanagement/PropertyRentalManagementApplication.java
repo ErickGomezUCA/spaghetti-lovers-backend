affd95fbbe2c586a227e2d82714eb0c911511110
MiriamDonas00022423 <00022423@uca.edu.sv>
2026-05-28 00:13:59 -0600

    feat: initialize Spring Boot backend project

---
+++ property-rental-management/src/main/java/com/example/propertyrentalmanagement/PropertyRentalManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.propertyrentalmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyRentalManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PropertyRentalManagementApplication.class, args);
+    }
+
+}
