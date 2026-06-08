d68dbcef1a077b0aa964beb97c8c8d30b289c369
MiriamDonas00022423 <00022423@uca.edu.sv>
2026-06-07 22:17:49 -0600

    feat: add access code service

---
+++ property-rental-management/src/main/java/com/example/propertyrentalmanagement/services/AccessCodeService.java
@@ -0,0 +1,15 @@
+package com.example.propertyrentalmanagement.services;
+
+import com.example.propertyrentalmanagement.dto.response.AccessCodeResponse;
+import com.example.propertyrentalmanagement.entitites.Reservation;
+
+import java.util.UUID;
+
+public interface AccessCodeService {
+
+    AccessCodeResponse generateAccessCodeForReservation(Reservation reservation);
+
+    AccessCodeResponse getActiveAccessCodeByReservationId(UUID reservationId);
+
+    void invalidateCodesByReservation(Reservation reservation);
+}
